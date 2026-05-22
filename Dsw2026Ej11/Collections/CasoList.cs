7f788f1bdfbfc1b8205ec918c71ad056c34b32e0
nahuel1111 <ricardo-nahuel@hotmail.com>
2026-05-22 00:00:47 -0300

    se crearon los metodos de casolist

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +11,38 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> _alumnos = new List<Alumno>();
+    public void AgregarAlumno(Alumno alumno)
+    {
+        if(alumno != null)
+        {
+            _alumnos.Add(alumno);
+        }
+    }
+    public List<Alumno> ObtenerAlumnos()
+    {
+        return _alumnos;
+    }
+   
+    public Alumno BuscarPorNombre(string nombre)
+    {
+        return _alumnos.FirstOrDefault(a => a.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public bool EliminarAlumno(Alumno alumno)
+    {
+        return _alumnos.Remove(alumno);
+    }
+
+    public bool EliminarPorPosicion (int indice)
+    {
+        if (indice >= 0 && indice < _alumnos.Count)
+        {
+            _alumnos.RemoveAt(indice);
+            return true;
+        }
+        return false;
+    }
+    
+
 }
